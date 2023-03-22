d2cfc33b9e1053c03186d9ab62d55d47282b6954
abzaman7 <abzaman7@gemail.com>
2023-03-22 19:46:51 +0600

    Updated

---
+++ zaman/Java/testJava/src/test/java/com/abzaman/testJava/SwitchCase.java
@@ -0,0 +1,42 @@
+package com.abzaman.testJava;
+
+public class SwitchCase {
+
+
+    public static void main(String[] args) {
+
+        int day = 7;
+
+        for (int i= 0; i<=7; i++){
+
+            switch (i){
+
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+
+            }
+
+        }
+
+
+    }
+}
