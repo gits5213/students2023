8e8a7da8fefcb3fe02c0485d4dbbdba4f78f26a6
md shahnewaz zaman <mzaman@resqsoft.com>
2023-03-29 08:51:20 -0400

    method added

---
+++ sheba/src/test/java/com/gits/sheba/java/objectClass/Method.java
@@ -0,0 +1,35 @@
+package com.gits.sheba.java.objectClass;
+
+public class Method {
+    /*
+    A method is a block of code or collection of statements or a set of code
+    grouped together to perform a certain task or operation.
+     */
+
+    //Access modifier
+    // return type
+    //Method name
+    //Parameter list/not
+    //Method body or method signature
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    function myMethod() {
+        return a+b;
+    }
+
+    myMethod() => {
+        return a+b;
+    }
+
+    def myFunction():
+
+    public static void main(String[] args) {
+        System.out.println(sum(2, 2));
+    }
+
+    //TestNG
+    //JUnit
+
+}
