f5fc1931e1bc250cb939b4f22ac1ac6f5ed2d60a
Mridul Biswas <mridulbiswas281@gmail.com>
2023-04-14 03:21:38 +0600

    selenium practice test

---
+++ mridul/devsden/src/test/java/com/gits/herokuapp/java/InvalidUsernamePassword.java
@@ -0,0 +1,60 @@
+package com.gits.herokuapp.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class InvalidUsernamePassword {
+    String expectedLoginErrorMessage = "Your password is invalid!\n" +
+            "×";
+
+    @Test
+    public void InvalidUsernamePassword () throws InterruptedException {
+
+        // Precondition - Chrome browser open
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the URL https://the-internet.herokuapp.com/
+        driver.get("https://the-internet.herokuapp.com");
+
+        // Click on Form authentication link
+        WebElement formAuthentication = driver.findElement(By.cssSelector("li:nth-of-type(21) > a"));
+        formAuthentication.click();
+        Thread.sleep(1000);
+
+        // Enter invalid username
+        WebElement username = driver.findElement(By.cssSelector("#username"));
+        username.sendKeys("SuperSecretPassword!");
+        Thread.sleep(1000);
+
+        // Enter invalid password
+        WebElement invalidpassword = driver.findElement(By.cssSelector("#password"));
+        invalidpassword.sendKeys("tomsmith");
+        Thread.sleep(1000);
+
+        // Click on login button
+        WebElement loginbuttonclick = driver.findElement(By.cssSelector("#login > button"));
+        loginbuttonclick.click();
+        Thread.sleep(1000);
+
+        // Validate login error message
+        WebElement errorMessage = driver.findElement(By.cssSelector("#flash"));
+        String actualErrorMessageText = errorMessage.getText();
+        Assert.assertEquals(actualErrorMessageText, expectedLoginErrorMessage);
+
+        // browser closes
+        driver.close();
+
+        // WebDriver close
+        driver.quit();
+
+    }
+
+
+
+
+
+}
---
+++ mridul/devsden/src/test/java/com/gits/herokuapp/java/ValidUsernamePassword.java
@@ -0,0 +1,53 @@
+package com.gits.herokuapp.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ValidUsernamePassword {
+    String expectedLoginErrorMessage = "Your password is invalid!\n" +
+            "×";
+    @Test
+    public void ValidUsernamePassword() throws InterruptedException {
+
+        // Precondition - Chrome browser open
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the URL https://the-internet.herokuapp.com/
+        driver.get("https://the-internet.herokuapp.com/");
+
+        // Click on Form authentication link
+        WebElement formAuthentication = driver.findElement(By.cssSelector("li:nth-of-type(21) > a"));
+        formAuthentication.click();
+        Thread.sleep(1000);
+
+        // Enter valid username
+        WebElement username = driver.findElement(By.cssSelector("#username"));
+        username.sendKeys("tomsmith");
+        Thread.sleep(1000);
+
+        // Enter valid password
+        WebElement invalidpassword = driver.findElement(By.cssSelector("#password"));
+        invalidpassword.sendKeys("SuperSecretPassword");
+        Thread.sleep(1000);
+
+        // Click on login button
+        WebElement loginbuttonclick = driver.findElement(By.cssSelector("#login > button"));
+        loginbuttonclick.click();
+        Thread.sleep(1000);
+
+        // Validate login successful message
+        WebElement errorMessage = driver.findElement(By.cssSelector("#flash"));
+        String actualErrorMessageText = errorMessage.getText();
+        Assert.assertEquals(actualErrorMessageText, expectedLoginErrorMessage);
+
+        // browser close
+        driver.close();
+        // WebDriver close
+        driver.quit();
+    }
+
+}
