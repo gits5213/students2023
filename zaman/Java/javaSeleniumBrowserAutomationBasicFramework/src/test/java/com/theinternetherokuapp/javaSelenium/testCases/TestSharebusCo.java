8dace2041ed6dd534049835e3457e784ac3d61ef
abzaman7 <abzaman7@gemail.com>
2023-04-23 07:17:38 +0600

    updated

--- zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/TestSharebusCo.java
+++ zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/TestSharebusCo.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.security.Key;
+import java.sql.Driver;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -38,6 +39,22 @@ public class TestSharebusCo {
 
 
         return wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+    //Custom function with explicit wait time for element to be clickable and click
+    public static void waitForElementToBeClickableAndClick(WebDriver driver, By by, int timeoutInSeconds) {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeoutInSeconds))
+                .pollingEvery(Duration.ofMillis(1000))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(ElementNotInteractableException.class)
+                .ignoring(ElementClickInterceptedException.class)
+                .ignoring(WebDriverException.class)
+                .ignoring(TimeoutException.class);
+
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(by));
+        element.click();
+
     }
 
     //Custom function with explicit wait time for element to be visible
@@ -88,13 +105,24 @@ public class TestSharebusCo {
     //Custom function for scrolling to specific element by locator
     public static void scrollToElement(WebDriver driver, By by) {
         WebElement element = driver.findElement(by);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'center', behavior: 'smooth'});", element);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'center', behavior: 'smooth'});", element);;
     }
+
+    //Custom function for scrolling to specific pixel
+    public static void scrollVertically(WebDriver driver, int pixels) {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, arguments[0]);", pixels);
+    }
+
     //JavaScriptExecutor for clicking hidden element
     public static void clickHiddenElement(WebDriver driver, By by) {
         WebElement element = driver.findElement(by);
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("arguments[0].click();", element);
+    }
+
+    public static void clickOnBody(WebDriver driver){
+
+        WebElement click = driver.findElement(By.tagName("body"));
     }
 
 
@@ -116,7 +144,7 @@ public class TestSharebusCo {
         //click login
         //Thread.sleep(3000);
         //waitForElementToDisappear(driver,By.cssSelector("#app > div.position-absolute.vw-100.vh-100.d-flex.align-items-center.overlay.flex-column.justify-content-center.position-fixed.overlay-primary-background"));
-        //waitForElementToBeClickable(driver,By.cssSelector(".btn.btn-sm.ms-2.sb-btn-md.sb-btn-primary.text-white"),10);
+        //waitForElementToBeClickableAndClick(driver,By.cssSelector(".btn.btn-sm.ms-2.sb-btn-md.sb-btn-primary.text-white"),10);
         driver.findElement(By.cssSelector(".btn.btn-sm.ms-2.sb-btn-md.sb-btn-primary.text-white")).click();
         driver.findElement(By.cssSelector(".btn.fw-normal.px-3.py-1.rounded-pill.sb-btn-lg.sb-btn-secondary")).click();
 
@@ -130,7 +158,7 @@ public class TestSharebusCo {
         driver.findElement(By.cssSelector(".btn.fw-bold.rounded-pill.sb-btn-lg.sb-btn-primary.w-100")).click();
 
         //dropdown click
-        waitForElementToBeClickable(driver,By.cssSelector(".p-component.p-dropdown.p-inputwrapper.p-inputwrapper-filled.w-50 > div[role='button']"), 10).click();
+        waitForElementToBeClickableAndClick(driver,By.cssSelector(".p-component.p-dropdown.p-inputwrapper.p-inputwrapper-filled.w-50 > div[role='button']"), 10);
 
         //Select Sharelead
         driver.findElement(By.cssSelector("ul[role='listbox'] > li:nth-of-type(2)")).click();
@@ -141,8 +169,8 @@ public class TestSharebusCo {
         //Set up a share bus click
 //        Thread.sleep(2000);
         waitForElementToDisappear(driver,By.cssSelector("#app > div.position-absolute.vw-100.vh-100.d-flex.align-items-center.overlay.flex-column.justify-content-center.position-fixed.overlay-primary-background"));
-//        waitForElementToBeClickable(driver, By.cssSelector("[class='btn btn-primary sb-btn-primary sb-btn-lg px-4 py-1 my-2 rounded-pill border-0 fw-400']"), 10).click();
-        driver.findElement(By.cssSelector("[class='btn btn-primary sb-btn-primary sb-btn-lg px-4 py-1 my-2 rounded-pill border-0 fw-400']")).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[class='btn btn-primary sb-btn-primary sb-btn-lg px-4 py-1 my-2 rounded-pill border-0 fw-400']"), 20);
+//        driver.findElement(By.cssSelector("[class='btn btn-primary sb-btn-primary sb-btn-lg px-4 py-1 my-2 rounded-pill border-0 fw-400']")).click();
 
         //enter From
         //Thread.sleep(2000);
@@ -151,57 +179,57 @@ public class TestSharebusCo {
         driver.findElement(By.cssSelector("[class='my-3'] [type]")).sendKeys("Oslo, Norwa");
         Thread.sleep(2000);
         driver.findElement(By.cssSelector("[class='my-3'] [type]")).sendKeys(Keys.SPACE);
-        waitForElementToBeClickable(driver, By.cssSelector("body > div:nth-child(6) > div:nth-child(2)"), 20).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("body > div:nth-child(6) > div:nth-child(2)"), 20);
 
         //enter To
         //Thread.sleep(2000);
         scrollToElement(driver,By.cssSelector("form div:nth-child(3) [type='text']"));
-        waitForElementToBeClickable(driver,By.cssSelector("form div:nth-child(3) [type='text']"),10).click();
+        scrollVertically(driver,100);
+        waitForElementToBeClickableAndClick(driver,By.cssSelector("form div:nth-child(3) [type='text']"),10);
         driver.findElement(By.cssSelector("form div:nth-child(3) [type='text']")).sendKeys("Kolbotn, Norwa");
         Thread.sleep(2000);
         driver.findElement(By.cssSelector("form div:nth-child(3) [type='text']")).sendKeys(Keys.SPACE);
-        waitForElementToBeClickable(driver, By.cssSelector("body > div:nth-child(7) > div:nth-child(1)"), 10).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("body > div:nth-child(7) > div:nth-child(1)"), 10);
 
         //select departure
         //Thread.sleep(2000);
         scrollToElement(driver,By.cssSelector("[placeholder='Departure Date']"));
-        waitForElementToBeClickable(driver, By.cssSelector("[placeholder='Departure Date']"), 10).click();
-        clickHiddenElement(driver,By.cssSelector("tr:nth-of-type(4) > td:nth-of-type(6)"));
-        Thread.sleep(5000);
-        waitForElementToBeClickable(driver, By.cssSelector("[class='row my-3'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"), 10).click();
-        Thread.sleep(500);
+        scrollVertically(driver,100);
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[placeholder='Departure Date']"), 10);
+        clickHiddenElement(driver,By.cssSelector(".p-datepicker-next.p-link"));
+        waitForElementToBeClickableAndClick(driver,By.cssSelector("tr:nth-of-type(4) > td:nth-of-type(6)"),10);
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[class='row my-3'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"), 10);
         sendKeys(driver,By.cssSelector("[class='row my-3'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"), 10, "06:33");
 
 
         //select return
         //Thread.sleep(2000);
-        scrollToElement(driver,By.cssSelector("[placeholder='Return Date']"));
-        waitForElementToBeClickable(driver, By.cssSelector("[placeholder='Return Date']"), 10).click();
-        clickHiddenElement(driver,By.cssSelector("tr:nth-of-type(5) > td:nth-of-type(3)"));
-        Thread.sleep(500);
-        waitForElementToBeClickable(driver, By.cssSelector("[class='row my-4'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"), 10).click();
-        Thread.sleep(500);
+        scrollToElement(driver,By.cssSelector(".my-4.row .p-component.p-inputtext.pv-datepicker-input"));
+        waitForElementToBeClickableAndClick(driver,By.cssSelector(".my-4.row .p-component.p-inputtext.pv-datepicker-input"), 10);
+        clickHiddenElement(driver,By.cssSelector(".p-datepicker-next.p-link"));
+        waitForElementToBeClickableAndClick(driver,By.cssSelector("tr:nth-of-type(5) > td:nth-of-type(3)"),10);
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[class='row my-4'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"), 10);
         sendKeys(driver,By.cssSelector("[class='row my-4'] [class='col-sm-12 col-md-6']:nth-of-type(1) [placeholder='Time']"),10, "06:33");
 
         //Click continue
         scrollToElement(driver,By.cssSelector(".align-items-center.border-0.btn.d-flex.fw-bold.justify-content-center.rounded-pill.sb-btn-lg.sb-btn-primary"));
-        waitForElementToBeClickable(driver, By.cssSelector(".align-items-center.border-0.btn.d-flex.fw-bold.justify-content-center.rounded-pill.sb-btn-lg.sb-btn-primary"), 10).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector(".align-items-center.border-0.btn.d-flex.fw-bold.justify-content-center.rounded-pill.sb-btn-lg.sb-btn-primary"), 10);
 
         //Click Yes Set up a Sharebus
         //Thread.sleep(7000);
-        waitForElementToBeClickable(driver, By.cssSelector(".border-light"), 20).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector(".border-light"), 20);
         driver.findElement(By.cssSelector(".fi-chevron-down")).click();
         driver.findElement(By.cssSelector(".results > ul > li:nth-of-type(4)")).click();
         driver.findElement(By.cssSelector("[type='submit']")).click();
 
         //Click create share bus
         //Thread.sleep(4000);
-        waitForElementToBeClickable(driver, By.cssSelector(".btn.btn-discount-size.gray-white-bg.rounded-end.ship-gray"), 10).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector(".btn.btn-discount-size.gray-white-bg.rounded-end.ship-gray"), 10);
         //Thread.sleep(4000);
         scrollToElement(driver,By.cssSelector("[class='discount-ticket-wrapper p-4 light-gray-bg mb-2 mt-2'] .rounded-end"));
-        waitForElementToBeClickable(driver, By.cssSelector("[class='discount-ticket-wrapper p-4 light-gray-bg mb-2 mt-2'] .rounded-end"), 10).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[class='discount-ticket-wrapper p-4 light-gray-bg mb-2 mt-2'] .rounded-end"), 10);
         //Thread.sleep(4000);
-        waitForElementToBeClickable(driver, By.cssSelector("[type='submit']"), 10).click();
+        waitForElementToBeClickableAndClick(driver, By.cssSelector("[type='submit']"), 10);
 
         //Click publish
         //Thread.sleep(4000);
