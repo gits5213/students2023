ce6daa2fd3947027f816e0f39a9edddd5f1675e1
abzaman7 <abzaman7@gemail.com>
2023-05-04 12:55:02 +0600

    FileUploader

--- zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/codeBackups/BrokenImages.java
+++ zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/codeBackups/BrokenImages.java
@@ -1,2 +1,34 @@
-package com.theinternetherokuapp.javaSelenium.testCases.codeBackups;public class BrokenImages {
+package com.theinternetherokuapp.javaSelenium.testCases.codeBackups;
+
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class BrokenImages {
+
+    public void brokenImageValidation(List<WebElement> elements){
+        int totalNumberOfBrokenImage = 0;
+        for (WebElement brokenImage : elements) {
+            String imageURL = brokenImage.getAttribute("src");
+            try{
+                URL url = new URL(imageURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(5000);
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode != 200) {
+                    System.out.println("Broken image found at" + imageURL);
+                    totalNumberOfBrokenImage++;
+                }
+                connection.disconnect();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Total number of broken images = " + totalNumberOfBrokenImage );
+    }
 }
--- zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/codeBackups/BrokenLinks.java
+++ zaman/Java/javaSeleniumBrowserAutomationBasicFramework/src/test/java/com/theinternetherokuapp/javaSelenium/testCases/codeBackups/BrokenLinks.java
@@ -1,2 +1,76 @@
-package com.theinternetherokuapp.javaSelenium.testCases.codeBackups;public class BrokenLinks {
+package com.theinternetherokuapp.javaSelenium.testCases.codeBackups;
+
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BrokenLinks {
+
+    public static void brokenLinks( List<WebElement> links) {
+
+        //the Set method is used to store unique value in a hash set data structure(a method to avoid duplicates)
+        Set<String> brokenLinkUrls = new HashSet<>();
+
+        for (WebElement link : links) {
+
+            try{
+                String linkURL = link.getAttribute("href");
+                URL url = new URL(linkURL);
+                URLConnection urlConnection = url.openConnection();
+                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+                if(httpURLConnection.getResponseCode() != 200)
+                {
+                    brokenLinkUrls.add(linkURL + " " + httpURLConnection.getResponseCode() + httpURLConnection.getResponseMessage());
+                }
+                httpURLConnection.disconnect();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        for (String brokenLinkUrl : brokenLinkUrls) {
+            System.err.println(brokenLinkUrl);
+
+        }
+    }
+
+//    				System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
+//                  System.err.println(linkURL + " - " + httpURLConnection.getResponseCode() + " - " + httpURLConnection.getResponseMessage());
+
+    public static void brokenLinksWithProxy( List<WebElement> links) {
+
+        //the Set method is used to store unique value in a hash set data structure(a method to avoid duplicates)
+        Set<String> brokenLinkUrls = new HashSet<>();
+
+        for (WebElement link : links) {
+
+            try{
+                String linkURL = link.getAttribute("href");
+                Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP, new InetSocketAddress("hostname", 80));
+                URL url = new URL(linkURL);
+                URLConnection urlConnection = url.openConnection(proxy);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+                if(httpURLConnection.getResponseCode() != 200)
+                {
+                    brokenLinkUrls.add(linkURL + " " + httpURLConnection.getResponseCode() + httpURLConnection.getResponseMessage());
+                }
+                httpURLConnection.disconnect();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        for (String brokenLinkUrl : brokenLinkUrls) {
+            System.err.println(brokenLinkUrl);
+
+        }
+    }
 }
